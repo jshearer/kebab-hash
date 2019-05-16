9810ee5bcb83ab4138c1f99914e9044601a06317
jshearer <joseph@narwhalsystems.com>
2019-05-16 16:59:31 -0400

    Improve kebab hash

--- src/index.js
+++ src/index.js
@@ -12,7 +12,13 @@ const shortHash = (input: string, length: number = 3): string =>
     .substr(0, length);
 
 // Given `input` return the kebab case version with a short has appended.
-const kebabHash = (path: string, hashLength: number = 3): string =>
-  `${kebabCase(path)}-${shortHash(path, hashLength)}`;
+// Add a default path length limit and generate a useful path with the first (pathLength/2) characters and the last (pathLength/2) chars
+const kebabHash = (path: string, hashLength: number = 3, pathLength: number = 60): string => {
+  let mutated_path = kebabCase(path);
+  if(mutated_path.length > pathLength){
+    mutated_path = `${mutated_path.substr(0, pathLength/2)}-${mutated_path.substr(mutated_path.length-(pathLength/2), pathLength/2)}`
+  }
+  return `${mutated_path}-${shortHash(path, hashLength)}`;
+}
 
 export default kebabHash;
--- src/index.test.js
+++ src/index.test.js
@@ -19,3 +19,9 @@ describe("hashLength 5", () => {
     expect(kebabHash("/foo/bar", 5)).toBe("foo-bar-1df48");
   });
 });
+
+describe("pathLength 14", () => {
+  test("Slices properly", () => {
+    expect(kebabHash("hello/world/foo/bar", 3, 14)).toBe("hello-w-foo-bar-f3b")
+  })
+})
